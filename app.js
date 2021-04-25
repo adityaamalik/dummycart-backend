98f29f9bbf510de9e87057b87ec84929666d544c
Aditya Malik <aditya007@outlook.com>
2021-04-25 14:13:48 +0530

    include path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
